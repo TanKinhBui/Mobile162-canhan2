ebdf3f31a0449bded5fa4689c030c4bed367fda2
Tan Kinh Bui <51301974@hcmut.edu.vn>
2017-04-30 02:56:30 +0700

    update

--- M2obileCanhan2/app/src/main/java/com/example/tankinhbui/m2obilecanhan2/ListAdapter.java
+++ M2obileCanhan2/app/src/main/java/com/example/tankinhbui/m2obilecanhan2/ListAdapter.java
@@ -1,7 +1,6 @@
 package com.example.tankinhbui.m2obilecanhan2;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,23 +34,23 @@ public class ListAdapter extends ArrayAdapter<DateA> {
             TextView tv0 = (TextView) view.findViewById(R.id.iDate);
             tv0.setText("              Ngày sổ      " + p.getDate());
             TextView tv1 = (TextView) view.findViewById(R.id.igiai1);
-            tv1.setText("G1iả 1:   " +p.getGiai1());
+            tv1.setText("Giải 1:   " +p.getGiai1());
             TextView tv2 = (TextView) view.findViewById(R.id.igiai2);
-            tv2.setText("G1iả 2:   " +p.getGiai2());
+            tv2.setText("Giải 2:   " +p.getGiai2());
             TextView tv3 = (TextView) view.findViewById(R.id.igiai3);
-            tv3.setText("G1iả 3:   " + p.getGiai3());
+            tv3.setText("Giải 3:   " + p.getGiai3());
             TextView tv4 = (TextView) view.findViewById(R.id.igiai4);
-            tv4.setText("G1iả 4:   " +p.getGiai4());
+            tv4.setText("Giải 4:   " +p.getGiai4());
             TextView tv5 = (TextView) view.findViewById(R.id.igiai5);
-            tv5.setText("G1iả 5:   " +p.getGiai5());
+            tv5.setText("Giải 5:   " +p.getGiai5());
             TextView tv6 = (TextView) view.findViewById(R.id.igiai6);
-            tv6.setText("G1iả 6:   " +p.getGiai6());
+            tv6.setText("Giải 6:   " +p.getGiai6());
             TextView tv7 = (TextView) view.findViewById(R.id.igiai7);
-            tv7.setText("G1iả 7:   " +p.getGiai7());
+            tv7.setText("Giải 7:   " +p.getGiai7());
             TextView tv8 = (TextView) view.findViewById(R.id.igiai8);
-            tv8.setText("G1iả 8:   " +p.getGiai8());
+            tv8.setText("Giải 8:   " +p.getGiai8());
             TextView tvDB = (TextView) view.findViewById(R.id.igiaiDB);
-            tvDB.setText("G1iả :   " +p.getDB());
+            tvDB.setText("Giải :   " +p.getDB());
 
         return view;
     }
